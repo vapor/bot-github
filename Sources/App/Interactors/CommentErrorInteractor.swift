08ccdb81dca2dbf96eb8a86a152dc2893498d87d
twof <fabiobean2@gmail.com>
2019-03-05 23:11:59 -0800

    Cleaned up guards

--- Sources/App/Interactors/CommentErrorInteractor.swift
+++ Sources/App/Interactors/CommentErrorInteractor.swift
@@ -1,23 +1,24 @@
 import Vapor
 
 public struct CommentErrorInteractor {
-    func execute(on req: Request, build: CircleCIBuild, repo: String, message: String) -> Future<Response> {
+    func execute(
+        on req: Request,
+        build: CircleCIBuild,
+        repo: String,
+        message: String
+    ) -> Future<Response> {
         guard let github = try? req.make(GithubService.self) else {
             return req.future(
                 error: ServiceError(identifier: "Github", reason: "GithubService could not be created")
             )
         }
         
-        guard build.pullRequests.count > 0 else {
-            return req.future(error: Abort(HTTPStatus.ok))
-        }
-        let pullRequest = build.pullRequests[0]
-        
         guard
+            let pullRequest = build.pullRequests.first,
             let issueNumberString = pullRequest.url.absoluteString.split(separator: "/").last,
             let issueNumber = Int(issueNumberString)
         else {
-            return req.future(error: Abort(.notFound))
+            return req.future(error: Abort(.ok))
         }
         
         do {
